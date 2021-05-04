0c7567b59e944c13d13c02e455b11cfa8c19d523
Tony <tony.lbvre@gmail.com>
2021-05-04 18:18:04 +0200

    feat: testable firestore_client

--- core/firestore_client.py
+++ core/firestore_client.py
@@ -2,10 +2,12 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-    'projectId': "trentiemeciel",
-})
 
-db = firestore.Client()
+def db():
+    # Use the application default credentials
+    cred = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(cred, {
+        'projectId': "trentiemeciel",
+    })
+
+    return firestore.Client()
